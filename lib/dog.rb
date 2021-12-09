9543cd2cc9260821137d362a2e35948dacce746b
Goldie <goldiejackson25@yahoo.com>
2021-12-09 08:48:42 -0600

    commit tree 9dca2dea9904445418deeaa240a073d49e3fbb75

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,101 @@
+require 'pry'
+
 class Dog
 
+    attr_accessor :name, :breed, :id
+
+    def initialize(name:, breed:, id: nil)
+        @id = id
+        @name = name
+        @breed = breed
+    end
+
+    def self.all
+        sql = <<-SQL
+        SELECT * FROM dogs
+        SQL
+
+        DB[:conn].execute(sql).map {|row| self.new_from_db(row)}
+    end
+
+    def self.create_table
+        sql = <<-SQL
+            CREATE TABLE IF NOT EXISTS dogs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                breed TEXT
+            )
+            SQL
+            DB[:conn].execute(sql)
+    end
+
+    def self.drop_table
+        sql = <<-SQL
+        DROP TABLE dogs
+        SQL
+        DB[:conn].execute(sql)
+    end
+
+    def save
+        sql = <<-SQL
+            INSERT INTO dogs (name, breed)
+            VALUES (?, ?)
+        SQL
+
+        DB[:conn].execute(sql, @name, @breed)
+
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+
+        self
+    end
+
+    def self.create(name:, breed:)
+        dog = Dog.new(name: name, breed: breed)
+        dog.save
+    end
+
+    def self.new_from_db(row)
+        self.new(id: row[0], name: row[1], breed: row[2])
+    end
+
+    def self.find_by_name(name)
+        sql = <<-SQL
+            SELECT *
+            FROM dogs
+            WHERE name = ?
+            LIMIT 1
+        SQL
+
+        DB[:conn].execute(sql, name).map {
+            |row| 
+            self.new_from_db(row)
+        }.first
+    end
+
+    # def self.find(id)
+    #     sql = <<-SQL
+    #         SELECT *
+    #         FROM dogs
+    #         WHERE id = ?
+    #         LIMIT 1
+    #     SQL
+
+    #     DB[:conn].execute(sql, id).map {|row| self.new_from_db(row)}.first
+    # end
+
+    def self.find(id)
+        sql = "SELECT * FROM dogs WHERE id=? LIMIT 1"
+
+        self.new_from_db(DB[:conn].execute(sql, id).first)
+    end
+
+    def self.find_or_create_by(name, breed)
+        if self.find_by_name(name) == name
+            return self.find_by_name(name)
+        elsif
+            self.create(name, breed)
+        end
+    end
 end
+
+binding.pry
